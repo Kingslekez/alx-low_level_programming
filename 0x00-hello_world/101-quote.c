9a5d8601016ade1ab27dbc71ee4c7874aa733fa5
Kingsley Ekezie <ekezie10@gmail.com>
2022-09-08 16:41:56 -0700

    ahhhh

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,17 +2,13 @@
 
 /**
   * main - Entry point
+  *
   * Return: Always 1 (Success)
   */
 
 int main(void)
 
-{
-
-		char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-		write(2, str1, 59);
-
-		return (1);
-
+{ char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  write(2, str1, 59);
+  return (1);
 }

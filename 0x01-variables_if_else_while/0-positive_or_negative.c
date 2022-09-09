494fe7e3c9a0ae470b9c3d7a00c8f7030d0c006b
Kingsley Ekezie <ekezie10@gmail.com>
2022-09-09 15:09:31 -0700

    6443

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -21,9 +21,14 @@
          if (n == 0)
                  printf("%d is zero\n", n);
          else if (n < 0)
-                 printf("%d is negative\n", n);
+
+                 printf("%d 
+  is negative\n", n);
+
          else
-                 printf("%d is positive\n", n);
+                 printf("%d 
+		 
+  is positive\n", n);
 
          return (0);
  }

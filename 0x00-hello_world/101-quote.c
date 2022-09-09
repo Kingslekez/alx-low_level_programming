601d41f8539131294856e34a7661d93538ef516f
Kingsley Ekezie <ekezie10@gmail.com>
2022-09-08 17:04:07 -0700

    good

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,18 @@
-#include <unistd.h>
-
+#include <stdio.h>
 /**
-  * main - Entry point
-  *
-  * Return: Always 1 (Success)
-  */
+ * main - main function 
+ * 
+ * Return: end program
+ */
 
 int main(void)
 
-{ 
-	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-  
-	write(2, str1, 59);
+{
 
-      	return (1);
+char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stderr, "%s\n", n);
+
+	return (1);
+
 }

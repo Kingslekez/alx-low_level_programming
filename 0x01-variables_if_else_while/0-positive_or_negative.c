dfb904238d5391288322c56ac8816c82dff05df9
Kingsley Ekezie <ekezie10@gmail.com>
2022-09-10 23:38:46 -0700

    holy

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,18 +17,20 @@
 
 
          srand(time(0));
+
          n = rand() - RAND_MAX / 2;
+
          if (n == 0)
-                 printf("%d is zero\n", n);
+ 	{	
+                 printf("%d is zero\n", n);
+	}
+
          else if (n < 0)
-
-                 printf("%d 
-  is negative\n", n);
-
+ 	{
+	printf("%d is negative\n", n);
+	}
          else
-                 printf("%d 
-		 
-  is positive\n", n);
-
+ 	{
+                 printf("%d is positive\n", n);
+	}
          return (0);
- }

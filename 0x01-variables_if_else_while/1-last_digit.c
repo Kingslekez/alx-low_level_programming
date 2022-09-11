d75a4b9c3780bd5ef77e5c8971357c6b0931f1e9
Kingsley Ekezie <ekezie10@gmail.com>
2022-09-11 00:03:45 -0700

    hello

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,8 +14,8 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-        printf("Last digit of %d is %d ", n, n % 10);
+	
+	printf("Last digit of %d is %d ", n, n % 10);
 
 	if (n % 10 > 5)
 	{

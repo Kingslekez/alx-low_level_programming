b095890fa5904a8f7e0c7f6c703630f434ad49bf
Kingsley Ekezie <ekezie10@gmail.com>
2022-09-13 13:50:34 -0700

    number0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "_putchar.h"
+#include "main.h"
 
 /**
  * main -  prints _putchar, followed by a new line
@@ -7,13 +7,15 @@
 
 int main(void)
 {
-        char *sh = "_putchar";
-  
-         while (*sh) 
-         { 
-                 _putchar(*sh); 
-                 sh++; 
-         } 
-         _putchar('\n'); 
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+
 	return 0;
 }

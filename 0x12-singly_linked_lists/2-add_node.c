3b9f0cadcb58a608b1f9b74111df265241b29cac
Kingsley Ekezie <ekezie10@gmail.com>
2022-10-20 15:21:52 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,20 +9,18 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new;
-    size_t nchar;
+	list_t *new;
+	size_t nchar;
 
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-        return (NULL);
-
-    new->str = strdup(str);
-
-    for (nchar = 0; str[nchar]; nchar++)
-        ;
-
-    new->len = nchar;
-    new->next = *head;
-    *head = new;
-
-    return
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	for
+		(nchar = 0;
+		 str[nchar];
+		 nchar++);
+	new->len = nchar;
+	new->next = *head;
+	*head = new;
+	return;
